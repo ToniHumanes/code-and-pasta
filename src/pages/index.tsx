8939eab4ff18527c8cb1f10f430e6128ffbfab19
Antonio Humanes <antoniohumanespacheco@gmail.com>
2025-11-09 01:10:00 +0100

    Added image and Changed the  page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import type { ReactNode } from "react";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageHeader from "../components/feature/Homepage/Hero";
 import HomepageFeatures from "../components/feature/Homepage/Cards";
 
 export default function Home(): ReactNode {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={"Código & Pasta"}
+      description="En este espacio llamado Código & Pasta encontrarás apuntes técnicos, reflexiones sobre arquitectura y producto"
     >
       <HomepageHeader />
       <main>
