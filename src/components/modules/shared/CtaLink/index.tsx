69503ef5e08830738927057fe1595c4a4c929946
Antonio Humanes <antoniohumanespacheco@gmail.com>
2026-04-14 15:31:17 +0200

    Chage hero and show more button content

--- src/components/modules/shared/CtaLink/index.tsx
+++ src/components/modules/shared/CtaLink/index.tsx
@@ -3,15 +3,25 @@ import Link from "@docusaurus/Link";
 import clsx from "clsx";
 import styles from "./style.module.css";
 
+const CTA_LINK_KINDS = { primary: "primary", secondary: "secondary" } as const;
+
 type Props = {
   to: string;
   children: ReactNode;
   className?: string;
+  kind?: keyof typeof CTA_LINK_KINDS;
 };
 
-export const CtaLink = ({ to, children, className }: Props) => {
+export const CtaLink = ({ to, children, className, kind }: Props) => {
   return (
-    <Link className={clsx(styles.ctaLink, className)} to={to}>
+    <Link
+      className={clsx(
+        styles.ctaLink,
+        className,
+        kind && styles[`ctaLink--${kind}`],
+      )}
+      to={to}
+    >
       {children}
     </Link>
   );
